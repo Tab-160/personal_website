3b47bbc76446f684c2c92d4e568acbc78238a12c
Tab-160 <rab.greenup@gmail.com>
2026-03-07 10:44:05 -0800

    added bare bones of recipes getting addded to database

--- docs/postgres.sql
+++ docs/postgres.sql
@@ -18,3 +18,19 @@ WITH (oids = false);
 
 
 -- 2025-11-29 05:48:00 UTC
+
+
+-- recipe stuff
+
+CREATE TABLE "public"."recipes" (
+    "title" text,
+    "ingredients" text[],
+    "instructions" text[]
+)
+WITH (oids = false);
+
+
+INSERT INTO recipes
+    VALUES ('Milk',
+           '{"1 Cup Milk"}',
+           '{"Add milk to cup", "Stir"}');
